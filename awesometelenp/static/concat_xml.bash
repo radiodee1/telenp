71adcdc881512d66646c25572b1fcf1655d0b4f3
David Liebman <david.c.liebman@gmail.com>
2013-12-22 16:52:48 -0500

    new concat-test.html file

--- awesometelenp/static/concat_xml.bash
+++ awesometelenp/static/concat_xml.bash
@@ -8,4 +8,12 @@ cat xml-parts_c.txt >> app.xml
 cat test.html >> app.xml
 cat xml-parts_d.txt >> app.xml
 
-echo "concat finished"
+echo "concat app.xml finished"
+
+
+cat xml-parts_b.txt > concat-test.html
+cat app.js >> concat-test.html
+cat xml-parts_c.txt >> concat-test.html
+cat test.html >> concat-test.html
+
+echo "concat concat-test.html finished"
