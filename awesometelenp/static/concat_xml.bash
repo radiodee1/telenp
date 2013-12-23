034ef068cedd77fbd6405e21674ab1a14a144a05
David Liebman <david.c.liebman@gmail.com>
2013-12-23 08:22:11 -0500

    changed names and bash file

--- awesometelenp/static/concat_xml.bash
+++ awesometelenp/static/concat_xml.bash
@@ -5,15 +5,15 @@ cat xml-parts_a.txt > app.xml
 cat xml-parts_b.txt >> app.xml
 cat app.js >> app.xml
 cat xml-parts_c.txt >> app.xml
-cat test.html >> app.xml
+cat app.html >> app.xml
 cat xml-parts_d.txt >> app.xml
 
 echo "concat app.xml finished"
 
 
-cat xml-parts_b.txt > concat-test.html
-cat app.js >> concat-test.html
-cat xml-parts_c.txt >> concat-test.html
-cat test.html >> concat-test.html
+cat xml-parts_b.txt > test.html
+cat app.js >> test.html
+cat xml-parts_c.txt >> test.html
+cat app.html >> test.html
 
-echo "concat concat-test.html finished"
+echo "concat test.html finished"
