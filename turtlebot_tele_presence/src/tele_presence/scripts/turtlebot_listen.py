80526403c2abd744ebea0b2ef8fb22d37d0da83a
David Liebman <david.c.liebman@gmail.com>
2013-12-27 16:43:06 -0500

    start working on seq following node

--- turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_listen.py
+++ turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_listen.py
@@ -10,24 +10,24 @@ from geometry_msgs.msg import Vector3
 def listen():
     rospy.Subscriber("instructions/command_velocity", TwistStamped, callback)
     pub_string = rospy.Publisher('chatter', String)
-    pub_move = rospy.Publisher('/mobile_base/commands/velocity', Twist)
     rospy.init_node('turtlebot_listen', anonymous=True)
     while not rospy.is_shutdown():
         str = "hello world %s" % rospy.get_time()
         rospy.loginfo(str)
         pub_string.publish(String(str))
-        twist = Twist();
-        twist.linear.x = 33;
-        twist.linear.y = 22;
-        twist.linear.z = 11;
-        twist.angular.x = 44;
-        twist.angular.y = 55;
-        twist.angular.z = 66;
-        pub_move.publish(twist)
         rospy.sleep(1.0)
 
 def callback(data):
+    pub_move = rospy.Publisher('/mobile_base/commands/velocity', Twist)
     rospy.loginfo(rospy.get_name() + ": I heard %s" % data.header.seq)
+    twist = Twist();
+    twist.linear.x = data.twist.linear.x;
+    twist.linear.y = 0;
+    twist.linear.z = 0;
+    twist.angular.x = 0;
+    twist.angular.y = 0;
+    twist.angular.z = data.twist.angular.z;
+    pub_move.publish(twist)
 
 
 def listener_x():
