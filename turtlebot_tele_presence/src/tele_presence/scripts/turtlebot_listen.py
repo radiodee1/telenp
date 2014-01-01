276686a3a1a589e15e12d94f2ec9abdbaf0341f1
David Liebman <david.c.liebman@gmail.com>
2014-01-01 18:29:44 -0500

    more depth finder code

--- turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_listen.py
+++ turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_listen.py
@@ -9,10 +9,16 @@ from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import PointCloud2
 
 mod_base = 16
+boundary_depth = 1
+
 linear_x = 0
 angular_z = 0
 seq_counter = 0
 kinect_obstruction = False
+kinect_left = False
+kinect_middle = False
+kinect_right = False
+
 
 def listen():
     rospy.init_node('turtlebot_listen', anonymous=True)
@@ -65,11 +71,39 @@ def callback_move(data):
 def callback_kinect(data) :
     # no obstruction to start
     global kinect_obstruction
+    global kinect_left, kinect_right, kinect_middle
     # kinect_obstruction = True
     rospy.loginfo("kinect " + str(kinect_obstruction))
+    # pick a height
+    height =  int (data.height / 2)
+    # pick three x coords near front and center
+    left_x = int ( data.width * 3 / 8)
+    middle_x = int (data.width / 2)
+    right_x = int (data.width - ( data.width * 3 / 8 ))
+    # examine three points
+    left = read_depth (left_x, height, data)
+    middle = read_depth (middle_x, height, data)
+    right = read_depth (right_x, height, data)
+    # do stuff
+    if (left == -1 or middle == -1 or right == -1) :
+        rospy.loginfo("exit -- bad depth")
+        return
+    kinect_left = False
+    kinect_right = False
+    kinect_middle = False
+    if (left < boundary_depth) :
+        kinect_obstruction = True
+        kinect_left = True
+    if (middle < boundary_depth) :
+        kinect_obstruction = True
+        kinect_middle = True
+    if (right < boundary_depth) :
+        kinect_obstruction = True
+        kinect_right = True
+    # exit ?
 
 
-def read_depth(height, width, data) :
+def read_depth(width, height, data) :
     # read function
     if (height >= data.height) or (width >= data.width) :
         return -1
