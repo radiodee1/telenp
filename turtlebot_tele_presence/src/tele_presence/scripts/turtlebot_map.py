dc38331ebd9567192ac054d702d3990ed7a7ce28
David Liebman <david.c.liebman@gmail.com>
2014-01-23 17:38:35 -0500

    trying different config files

--- turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_map.py
+++ turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_map.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 import rospy
-#import numpy as np
-#import sensor_msgs.point_cloud2 as pc2
 
 import roslib
-#roslib.load_manifest('tele_presence') # map_store
+roslib.load_manifest('tele_presence') # map_store
 
 import sys
-import map_store.srv
-
+#import map_store.srv
+#import tele_presence
+from tele_presence.msg import * # MapListEntryList
 from nav_msgs.msg import *
 from nav_msgs.srv import *
 from map_store.srv import *
@@ -18,12 +17,7 @@ from roslib import message
 #from std_msgs.msg import String
 #from std_msgs.msg import Header
 #from std_msgs.msg import UInt8
-from tele_presence.msg import * # MapListEntryList
 
-#from geometry_msgs.msg import Twist
-#from geometry_msgs.msg import TwistStamped
-#from geometry_msgs.msg import Vector3
-#from sensor_msgs.msg import PointCloud2, PointField
 
 # type 'rosrun map_store map_manager' to start...
 # or 'roslaunch tele_presence full.launch' to start...
@@ -39,7 +33,7 @@ def map_stuff():
         rospy.sleep(1.0)
 
 def try_list():
-    pub_list = rospy.Publisher("/" + basename + '/map_list', MapListEntryList)
+    pub_list = rospy.Publisher("/" + basename + '/map_list', msg.MapListEntryList)
     list_message = MapListEntryList()
     #
     rospy.wait_for_service("/list_maps")
