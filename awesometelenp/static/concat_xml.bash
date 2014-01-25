96897b063d74f5b3ab2af1695fe4bbed32187900
David Liebman <david.c.liebman@gmail.com>
2014-01-25 16:18:18 -0500

    starting map wizard

--- awesometelenp/static/concat_xml.bash
+++ awesometelenp/static/concat_xml.bash
@@ -4,8 +4,10 @@
 cat xml-parts_a.txt > app.xml
 cat xml-parts_b.txt >> app.xml
 cat app.js >> app.xml
+cat app-map.js >> app.xml
 cat xml-parts_c.txt >> app.xml
 cat app.html >> app.xml
+cat app-map.html >> app.xml
 cat xml-parts_d.txt >> app.xml
 
 echo "concat app.xml finished"
@@ -13,7 +15,9 @@ echo "concat app.xml finished"
 
 cat xml-parts_b.txt > test.html
 cat app.js >> test.html
+cat app-map.js >> test.html
 cat xml-parts_c.txt >> test.html
 cat app.html >> test.html
+cat app-map.html >> test.html
 
 echo "concat test.html finished"
--- turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_map.py
+++ turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_map.py
@@ -15,16 +15,18 @@ from map_store.srv import *
 # or 'roslaunch tele_presence full.launch' to start...
 
 basename = "telenp"
-
+test_map = None #
+dynamic_map = rospy.Service('dynamic_map', GetMap, lambda x: GetMapResponse(map=test_map))
 
 def map_stuff():
     rospy.init_node('turtlebot_map', anonymous=True)
     if not rospy.is_shutdown():
         #
-        try_list()
-        save_a_map()
+        #try_list()
+        #save_a_map()
         print "-------------------"
-        try_list()
+        show_a_map()
+        #try_list()
         #rospy.sleep(1.0)
 
 def try_list():
@@ -39,50 +41,45 @@ def try_list():
         element.name = m.name
         element.id = m.map_id
         initial_map_list.append(element)
-        print "map: " , element.name
+        print "map: " , element.name, element.id
     print "done" 
     list_message.len = len(initial_map_list)
     list_message.list = initial_map_list
     pub_list.publish(list_message)
     
 def save_a_map() :
-    #if (list_message.len == 0) :
-        map_pub = rospy.Publisher('/map', OccupancyGrid)
-        newmap = create_map( 0 ) 
-        #print newmap
-        #Create services, publishers, and subscribers
-        #map_pub = rospy.Publisher('/map', OccupancyGrid)
-        map_pub.publish(newmap)
-        test_map_sub = rospy.Subscriber('/test_map', OccupancyGrid, on_map)
-        dynamic_map = rospy.Service('dynamic_map', GetMap, lambda x: GetMapResponse(map=newmap))
-        #
-        #print "Wait for /list_maps"
-        #rospy.wait_for_service("/list_maps")
-        #list_maps = rospy.ServiceProxy('/list_maps', map_store.srv.ListMaps)
-        
-        print "Wait for /name_latest_map"
-        rospy.wait_for_service("save_map")
-        save_map = rospy.ServiceProxy('save_map', SaveMap)
-        
-        print "latest"
-        #print "Wait for /delete_map"
-        #rospy.wait_for_service("/delete_map")
-        #delete_map = rospy.ServiceProxy('/delete_map', map_store.srv.DeleteMap)
-        #print "Wait for /rename_map"
-        #rospy.wait_for_service("/rename_map")
-        #rename_map = rospy.ServiceProxy('/rename_map', map_store.srv.RenameMap)
-        #print "Wait for /publish_map"
-        #rospy.wait_for_service("/publish_map")
-        #publish_map = rospy.ServiceProxy('/publish_map', map_store.srv.PublishMap)
-        #print "Wait for /test_dynamic_map"
-        #rospy.wait_for_service("/test_dynamic_map")
-        #test_dynamic_map = rospy.ServiceProxy('/test_dynamic_map', map_store.srv.GetMap)
-        #
-        #rospy.wait_for_service("/save_map")
-        #name_latest_map = rospy.ServiceProxy('/save_map', SaveMap)
-        saved_map_name = "test_map_srv234"
-        save_map(saved_map_name)
-        print "saved"
+    map_pub = rospy.Publisher('/map', OccupancyGrid)
+    newmap = create_map( 0 ) 
+    
+    map_pub.publish(newmap)
+    test_map_sub = rospy.Subscriber('/test_map', OccupancyGrid, on_map)
+    #
+    
+    print "Wait for /name_latest_map"
+    rospy.wait_for_service("save_map")
+    save_map = rospy.ServiceProxy('save_map', SaveMap)
+    
+    saved_map_name = "test_map_srv234"
+    save_map(saved_map_name)
+    print "saved"
+
+def show_a_map() :
+    # always first map
+    print "wait for list_maps"
+    rospy.wait_for_service("list_maps")
+    list_maps = rospy.ServiceProxy('list_maps', ListMaps)
+    #
+    print "done mapone" 
+    mapone = list_maps().map_list[0]
+    print mapone
+    print "------------"
+    print "Wait for /publish_map"
+    rospy.wait_for_service("/publish_map")
+    #
+    publish_map = rospy.ServiceProxy('/publish_map', PublishMap)
+    publish_map(mapone.map_id)
+    print "published"
+
 
 def on_map( data ) :
     print "found ", data
