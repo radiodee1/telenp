9eb3227cb105caad3cccbced2c59a5dde6fd6d7a
David Liebman <david.c.liebman@gmail.com>
2014-01-25 11:42:10 -0500

    save first map... map is test map.

--- turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_map.py
+++ turtlebot_tele_presence/src/tele_presence/scripts/turtlebot_map.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 import rospy
 
-#import roslib
-#roslib.load_manifest('tele_presence') # map_store
-
 import sys
-#import map_store.srv
-#import tele_presence
+
 from tele_presence.msg import MapListEntryList, MapListElement
 from nav_msgs.msg import *
 from nav_msgs.srv import *
 from map_store.srv import *
 
-from roslib import message
+#from roslib import message
 
 
 # type 'rosrun map_store map_manager' to start...
@@ -23,17 +19,20 @@ basename = "telenp"
 
 def map_stuff():
     rospy.init_node('turtlebot_map', anonymous=True)
-    while not rospy.is_shutdown():
+    if not rospy.is_shutdown():
         #
         try_list()
-        rospy.sleep(1.0)
+        save_a_map()
+        print "-------------------"
+        try_list()
+        #rospy.sleep(1.0)
 
 def try_list():
     pub_list = rospy.Publisher("/" + basename + '/map_list', MapListEntryList)
     list_message = MapListEntryList()
-    #
-    rospy.wait_for_service("/list_maps")
-    list_maps = rospy.ServiceProxy('/list_maps', map_store.srv.ListMaps)
+    print "wait for list_maps"
+    rospy.wait_for_service("list_maps")
+    list_maps = rospy.ServiceProxy('list_maps', ListMaps)
     initial_map_list = []
     for m in list_maps().map_list:
         element = MapListElement()
@@ -48,36 +47,42 @@ def try_list():
     
 def save_a_map() :
     #if (list_message.len == 0) :
+        map_pub = rospy.Publisher('/map', OccupancyGrid)
         newmap = create_map( 0 ) 
         #print newmap
         #Create services, publishers, and subscribers
-        map_pub = rospy.Publisher('/map', OccupancyGrid)
+        #map_pub = rospy.Publisher('/map', OccupancyGrid)
         map_pub.publish(newmap)
-        #test_map_sub = rospy.Subscriber('/test_map', OccupancyGrid, on_map)
-        #dynamic_map = rospy.Service('dynamic_map', GetMap, lambda x: GetMapResponse(map=test_map_2))
+        test_map_sub = rospy.Subscriber('/test_map', OccupancyGrid, on_map)
+        dynamic_map = rospy.Service('dynamic_map', GetMap, lambda x: GetMapResponse(map=newmap))
+        #
         #print "Wait for /list_maps"
         #rospy.wait_for_service("/list_maps")
         #list_maps = rospy.ServiceProxy('/list_maps', map_store.srv.ListMaps)
+        
         print "Wait for /name_latest_map"
-        rospy.wait_for_service("/save_map")
-        save_map = rospy.ServiceProxy('/save_map', map_store.srv.SaveMap)
+        rospy.wait_for_service("save_map")
+        save_map = rospy.ServiceProxy('save_map', SaveMap)
+        
+        print "latest"
         #print "Wait for /delete_map"
         #rospy.wait_for_service("/delete_map")
         #delete_map = rospy.ServiceProxy('/delete_map', map_store.srv.DeleteMap)
         #print "Wait for /rename_map"
         #rospy.wait_for_service("/rename_map")
         #rename_map = rospy.ServiceProxy('/rename_map', map_store.srv.RenameMap)
-        print "Wait for /publish_map"
-        rospy.wait_for_service("/publish_map")
-        publish_map = rospy.ServiceProxy('/publish_map', map_store.srv.PublishMap)
+        #print "Wait for /publish_map"
+        #rospy.wait_for_service("/publish_map")
+        #publish_map = rospy.ServiceProxy('/publish_map', map_store.srv.PublishMap)
         #print "Wait for /test_dynamic_map"
         #rospy.wait_for_service("/test_dynamic_map")
         #test_dynamic_map = rospy.ServiceProxy('/test_dynamic_map', map_store.srv.GetMap)
         #
         #rospy.wait_for_service("/save_map")
         #name_latest_map = rospy.ServiceProxy('/save_map', SaveMap)
-        saved_map_name = "test_map_srv"
-        save_map(map_name=saved_map_name)
+        saved_map_name = "test_map_srv234"
+        save_map(saved_map_name)
+        print "saved"
 
 def on_map( data ) :
     print "found ", data
