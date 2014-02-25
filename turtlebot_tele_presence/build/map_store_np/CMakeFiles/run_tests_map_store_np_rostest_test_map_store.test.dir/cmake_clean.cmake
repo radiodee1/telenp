545a4108fa6e5b781ec7ccd9496f95b434c8d3d3
David Liebman <david.c.liebman@gmail.com>
2014-02-25 12:05:09 -0500

    still working on compile

--- turtlebot_tele_presence/build/map_store_np/CMakeFiles/run_tests_map_store_np_rostest_test_map_store.test.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/run_tests_map_store_np_rostest_test_map_store.test"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/run_tests_map_store_np_rostest_test_map_store.test.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- turtlebot_tele_presence/build/map_store_np/CTestTestfile.cmake
+++ turtlebot_tele_presence/build/map_store_np/CTestTestfile.cmake
@@ -4,4 +4,3 @@
 # 
 # This file includes the relevent testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(_ctest_map_store_np_rostest_test_map_store.test "/home/dave/workspace/telenp/turtlebot_tele_presence/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/hydro/share/catkin/cmake/test/run_tests.py" "/home/dave/workspace/telenp/turtlebot_tele_presence/build/test_results/map_store_np/rostest-test_map_store.xml" "--return-code" "/opt/ros/hydro/share/rostest/cmake/../../../bin/rostest --pkgdir=/home/dave/workspace/telenp/turtlebot_tele_presence/src/map_store_np --package=map_store_np --results-filename test_map_store.xml /home/dave/workspace/telenp/turtlebot_tele_presence/src/map_store_np/test/map_store.test ")
